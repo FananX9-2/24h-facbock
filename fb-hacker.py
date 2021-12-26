575a2f5f9f62ec87a5303b513bd7f5f5b6865f9a
FananX9-2 <85710756+FananX9-2@users.noreply.github.com>
2021-12-26 18:12:18 +0100

    Add files via upload

---
+++ fb-hacker.py
@@ -0,0 +1,23 @@
+import os
+os.system ('pkg install cmatrix -y')
+os.system ('clear')
+os.system ('pkg install figlet -y')
+os.system ('clear')
+os.system ('figlet Fb-hacker-P')
+print ("")
+print ("")
+print ('\033[1;32m{1}\033[1;33m Hacker facebook 24h')
+print ("")
+print ("")
+print ('\033[1;32mFor Example, Choose a Number \033[1;31m1,2,')
+v7x2 = input (" \033[1;33mhacker\033[1;31m==> ")
+if v7x2 == '1' :
+        os.system ('clear')
+        print ('')
+        print ("Loading..........!")
+        print ('Only available in 24 hours')
+if v7x2 == '2' :
+        os.system('xdg-open https://shortest.link/HLw')
+
+
+
